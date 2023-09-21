b357a91626a4478fb7000f4a9b95d9989e1e06d4
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-09-21 20:39:38 +0300

    Major Views + Methods

---
+++ JS Web/Theory/02CatShelterExpress/catalog.js
@@ -0,0 +1,7 @@
+const catsCatalog = [
+    {name: 'Cherry', breed: 'Persian', description: 'Very fluffy cat with big claws.', imageUrl: 'https://i.ytimg.com/vi/YSHDBB6id4A/maxresdefault.jpg'},
+    {name: 'Berry', breed: 'Unknown', description: 'It is a cat which grew up on the streets, it is very teritorial.', imageUrl: 'https://static.boredpanda.com/blog/wp-content/uploads/2014/02/funny-wet-cats-coverimage.jpg'},
+    {name: 'Cherry', breed: 'Great Britain', description: 'Very lazy cat, which loves being worshiped.', imageUrl: 'https://elements-video-cover-images-0.imgix.net/files/794a0003-e012-4364-b723-fc4850cf9f80/inline_image_preview.jpg?auto=compress%2Cformat&fit=min&h=394&w=700&s=f9f3980089b837a5386610e6dd0b9431'}
+]
+
+exports.getCats = () => catsCatalog;
--- JS Web/Theory/02CatShelterExpress/index.js
+++ JS Web/Theory/02CatShelterExpress/index.js
@@ -1,10 +1,34 @@
-const express = require ("express");
+const express = require("express");
+const handlebars = require("express-handlebars");
 const PORT = 5050;
 const app = express();
+const path = require("path");
 
-app.get('/', (req, res) =>{
-    res.send("Cat's home page.")
+const { getCats } = require("./catalog.js");
 
-})
+//View Engine
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
 
+const staticFile = express.static("public");
+app.use(staticFile);
+
+app.get("/", (req, res) => {
+  const cats = getCats();
+    console.log(cats)
+  res.render("home", { layout: "mainHome", cats });
+});
+
+app.get("/cats/add-breed", (req, res) => {
+  res.render("add-breed");
+});
+
+app.get("/cats/add-cat", (req, res) => {
+  res.render("add-cat");
+});
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.send("<h1> The page you are looking for was Not Found!</h1>");
+});
 app.listen(PORT, () => console.log(`Express running on port: ${PORT}`));
