4c463be18a912449c36d8df7f61a26976f187402
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-09-24 20:51:47 +0300

    Modified Views
    Left only main tags

--- JS Web/Exercise/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS Web/Exercise/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-
+const path = require('path')
 const app = express();
 const CLIENT_PORT = 5050;
 
+//Handlebars Configuration
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
+
+// Set-up static files
+const staticFiles = express.static(path.resolve(__dirname, "public"));
+app.use(staticFiles);
+
 
 app.get("/", (req, res) => {
   res.render("index");
