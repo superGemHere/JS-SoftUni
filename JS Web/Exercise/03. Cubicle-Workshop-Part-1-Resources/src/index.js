f3d262744d5a30972e46f2e57d5f13ab46b1d864
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-09-24 20:42:04 +0300

    Added static html
    Home page

--- JS Web/Exercise/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS Web/Exercise/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const CLIENT_PORT = 5050;
 
-app.get('/', (req, res) => {
-    res.send("<h1>Home Page</h1>")
-})
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
 
-app.listen(CLIENT_PORT, () => console.log(`Express running on port: ${CLIENT_PORT}`))
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(CLIENT_PORT, () =>
+  console.log(`Express running on port: ${CLIENT_PORT}`)
+);
