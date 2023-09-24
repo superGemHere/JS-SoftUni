21c46ddd05f20282398a9a68c656bf7bf3d28fc8
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-09-24 20:10:40 +0300

    Initialized the App and port

--- JS Web/Exercise/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS Web/Exercise/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+
+const app = express();
+const CLIENT_PORT = 5050;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Home Page</h1>")
+})
+
+app.listen(CLIENT_PORT, () => console.log(`Express running on port: ${CLIENT_PORT}`))
