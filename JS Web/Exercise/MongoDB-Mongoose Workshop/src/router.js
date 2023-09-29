f7fa8fd900fac1402f090f9272fca398e12bd1d8
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-09-29 19:48:57 +0300

    add accessory controler with get accessory view

--- JS Web/Exercise/MongoDB-Mongoose Workshop/src/router.js
+++ JS Web/Exercise/MongoDB-Mongoose Workshop/src/router.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const homeControler = require("./controllers/homeControler");
 const cubeControler = require("./controllers/cubeControler");
+const accessoryControler = require('./controllers/accessoryControler')
 
 router.use(homeControler);
 
 router.use('/cubes', cubeControler);
+
+router.use('/acessories', accessoryControler)
 
 
 router.get('*', (req, res) =>{
