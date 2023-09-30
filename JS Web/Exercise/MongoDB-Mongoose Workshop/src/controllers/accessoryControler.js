e3a692b758854a27048e5ea0be539419eee9069e
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-09-30 19:45:39 +0300

    Added Create Accessory functionality

--- JS Web/Exercise/MongoDB-Mongoose Workshop/src/controllers/accessoryControler.js
+++ JS Web/Exercise/MongoDB-Mongoose Workshop/src/controllers/accessoryControler.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
+const accessoryService = require("../services/accessoryService");
 
 router.get("/create", (req, res) => {
   res.render("accessory/create");
 });
 
-router.post("/create", (req, res) => {
-  const {name, description, imageUrl} = req.body;
+router.post("/create", async (req, res) => {
+  const { name, description, imageUrl } = req.body;
+  await accessoryService.create({ name, description, imageUrl });
+
+  res.redirect('/');
 });
 
 module.exports = router;
