216dd6679855bd960149c17aeb3078741875718a
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-10-05 22:17:57 +0300

    add register page

--- JS Web/Exercise/Session and authentication W-shop/src/router.js
+++ JS Web/Exercise/Session and authentication W-shop/src/router.js
@@ -1,17 +1,19 @@
 const router = require("express").Router();
 const homeControler = require("./controllers/homeControler");
 const cubeControler = require("./controllers/cubeControler");
-const accessoryControler = require('./controllers/accessoryControler')
+const accessoryControler = require("./controllers/accessoryControler");
+const userController = require("./controllers/userController");
 
 router.use(homeControler);
 
-router.use('/cubes', cubeControler);
+router.use("/cubes", cubeControler);
 
-router.use('/accessories', accessoryControler)
+router.use("/accessories", accessoryControler);
 
+router.use("/users", userController);
 
-router.get('*', (req, res) =>{
-    res.redirect('/404')
+router.get("*", (req, res) => {
+  res.redirect("/404");
 });
 
 module.exports = router;
