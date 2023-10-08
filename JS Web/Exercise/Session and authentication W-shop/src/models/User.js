8e03fa3c2b9ba04e960c90d571a67ce49af9f425
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-10-08 12:22:00 +0300

    save password with hash

--- JS Web/Exercise/Session and authentication W-shop/src/models/User.js
+++ JS Web/Exercise/Session and authentication W-shop/src/models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: {
-    type: String
+    type: String,
+    required: [true, 'Password field must be filled!']
     // validate: {
     //     validator: function(value){
     //         return this.repeatPassword === value;
@@ -19,5 +21,10 @@ userSchema.virtual("repeatPassword").set(function(value) {
   }
 });
 
+userSchema.pre('save', async function () {
+  const hash =  await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+})
 const User = mongoose.model('User',userSchema);
 module.exports = User;
