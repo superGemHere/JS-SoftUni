7a6ff7280cedded508a151a002b40cf30e6c029d
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-10-09 22:55:09 +0300

    Add Validations User model

--- JS Web/Exercise/Session and authentication W-shop/src/models/User.js
+++ JS Web/Exercise/Session and authentication W-shop/src/models/User.js
@@ -1,12 +1,25 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    minLength: [5, 'Username must be at least 5 characters long.'],
+    match: [/^[A-Za-z0-9]+$/, 'Username must be alphanumeric'],
+    unique: true
+  },
   password: {
     type: String,
-    required: [true, 'Password field must be filled!']
-  },
+    required: [true, "Password field must be filled!"],
+    validate: {
+      validator: function(value){
+        return /^[A-Za-z0-9]+$/.test(value);
+      },
+      message: 'Invalid password characters!'
+    },
+    minLength: [8, 'Password must be at least 8 characters long.'],
+  }
 });
 
 userSchema.virtual("repeatPassword").set(function(value) {
@@ -15,10 +28,10 @@ userSchema.virtual("repeatPassword").set(function(value) {
   }
 });
 
-userSchema.pre('save', async function () {
-  const hash =  await bcrypt.hash(this.password, 10);
+userSchema.pre("save", async function() {
+  const hash = await bcrypt.hash(this.password, 10);
 
   this.password = hash;
-})
-const User = mongoose.model('User',userSchema);
+});
+const User = mongoose.model("User", userSchema);
 module.exports = User;
